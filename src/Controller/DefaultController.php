96d7c7536ec799dd0fe27f32a3cae3f454145891
Adam <a.stankiewicz7@gmail.com>
2019-10-13 11:47:31 +0200

    Adding a dog / cat survey

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,9 +3,18 @@ namespace App\Controller;
 
 use App\Repository\AnimalRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Asset\Context\RequestStackContext;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\Stream;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class DefaultController extends BaseController
 {
+
     /**
      * @Route("/", name="app_homepage")
      */
@@ -63,5 +72,38 @@ class DefaultController extends BaseController
             // 'animals' => $animals,
             'title' => 'Kontakt'
         ]);
+    }
+
+
+    /**
+     * @Route("/ankieta-{slug}", name="app_other_questionnaire")
+     */
+    public function questionnaire($slug)
+    {
+        
+        // Retrieve the HTML generated in our twig file
+        return $this->_render('other/questionnaire.html.twig', [
+            'title' => "Ankieta " . $slug,
+            'name_pdf' => 'ankieta_'. $slug
+        ]);
+    }
+
+    /**
+     * @Route("/download-pdf-{name_pdf}", name="app_other_download_pdf")
+     */
+    public function downloadPdf($name_pdf)
+    {   
+        $file = new Stream('documents/pdf/' . $name_pdf . '.pdf');
+
+        try {
+            $response = new BinaryFileResponse($file);
+
+            $response->headers->set('Content-Type', 'application/pdf');
+            $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $name_pdf.'.pdf'); 
+            return $response;
+        } catch (FileException $e) {
+            throw new $e("Nie ma takiego pliku" );
+        }
+        
     }
 }
